3ed87d2a9ca7bf57b5c9c19440533e9f1ffda888
Al7rbi <atif7rbi@hotmail.com>
2026-09-05 03:45:02 +0300

    test(core): refuse unsafe database targets

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -10,9 +11,24 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
 
+        if (! app()->environment('testing')) {
+            throw new \RuntimeException(
+                'EduCore tests require APP_ENV=testing.'
+            );
+        }
+
         if (config('database.default') !== 'pgsql') {
             throw new \RuntimeException(
                 'EduCore Feature/Integration tests require PostgreSQL.'
+            );
+        }
+
+        $databaseName = DB::connection()->getDatabaseName();
+
+        if (! preg_match('/(?:^|[_-])(test|testing)(?:[_-]|$)/i', $databaseName)) {
+            throw new \RuntimeException(
+                'Unsafe EduCore test database target: '.$databaseName
+                .'. Test databases must include test or testing in their name.'
             );
         }
     }
