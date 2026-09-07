da9df39a4b1b4170a08f1aeab91b41faa5329fff
Al7rbi <atif7rbi@hotmail.com>
2026-09-07 04:55:35 +0300

    fix(ui): embed favicon to bypass stale icon handling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,14 +16,9 @@
         <title>EduCore</title>
         <link
             rel="icon"
-            href="/favicon.svg?v=2"
             type="image/svg+xml"
             sizes="any"
-        >
-        <link
-            rel="shortcut icon"
-            href="/favicon.svg?v=2"
-            type="image/svg+xml"
+            href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'%3E%3Crect width='64' height='64' rx='14' fill='%23eef8ff'/%3E%3Cpath d='M12 26 32 14l20 12-20 12L12 26Z' fill='%230877c9'/%3E%3Cpath d='M20 35v9c7 5 17 5 24 0v-9l-12 7-12-7Z' fill='%230663a8'/%3E%3C/svg%3E"
         >
 
         @vite('resources/frontend/main.tsx')
